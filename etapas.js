3bfe38ab39c7d77f1d7c51f1f31c71cefedaa5ee
Sistemas Tecnocomp <sistemas.tecnocomp@tecnocomp.com.br>
2022-04-05 23:16:40 -0300

    First commit at Urna Eletronica

---
+++ etapas.js
@@ -0,0 +1,13 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero:38111,
+            name: "Fulano de Tal",
+            partido: 'ABC',
+        }
+    ]
+    }
+]
